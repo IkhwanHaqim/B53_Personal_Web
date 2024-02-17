ab51e940cb9d6c223cb678e017e54ee23aee5fd1
Ikhwan <ikhwanhakim756@gmail.com>
2024-02-17 16:11:40 +0700

    Calculate Duration

--- assets/js/my-project.js
+++ assets/js/my-project.js
@@ -3,22 +3,38 @@ const blogs = [];
 function addBlog(e) {
     e.preventDefault()
 
+    // bagian duration
+    const calculateDuration = () => {
+      const startDate = new Date (document.getElementById("startDate").value)
+      const endDate = new Date (document.getElementById("endDate").value)
+
+    startDate.setHours(0,0,0,0)
+    endDate.setHours(0,0,0,0)
+    const oneDay = 24 * 60 * 60 * 1000;
+    const difference = Math.abs(endDate-startDate);
+    const results =Math.round(difference/oneDay);
+
+    return results;
+    }
+    const duration = calculateDuration();
+
     const name = document.getElementById("name").value
     const content = document.getElementById("content").value
     const nodejs = document.getElementById("nodejs").checked
     const reactjs = document.getElementById("reactjs").checked
     const angular = document.getElementById("angular").checked
     const vuejs = document.getElementById("vuejs").checked
-    
+
     const blog = {
         name,
         content,
+        duration,
         nodejs,
         reactjs,
         angular,
         vuejs
     }
-    
+
 
     blogs.push(blog)
     renderBlog()
@@ -31,10 +47,10 @@ function renderBlog() {
     for(let index = 0; index < blogs.length; index++) {
         html += `
         <div class="card">
-            <img src="./assets/img/prince.jpg" class="card-image" />
+            <img src="./assets/img/tiger.jpg" class="card-image" />
             <a href="project-detail.html" target="_blank">${blogs[index].name} </a>
         <div class="duration">
-        Duration: 
+        Duration: ${blogs[index].duration} day
         </div>
         <p class="content">
             ${blogs[index].content}
